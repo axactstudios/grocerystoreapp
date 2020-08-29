b4fa3d3d6cb2a885c47cd15563f51d473686f1d0
Chitransh Jain <cjsmcs2810@gmail.com>
2020-08-29 16:47:30 +0530

    shop registration done

---
+++ lib/Screens/NavBarScreens/CompletedOrders.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CompletedOrders extends StatefulWidget {
+  @override
+  _CompletedOrdersState createState() => _CompletedOrdersState();
+}
+
+class _CompletedOrdersState extends State<CompletedOrders> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/Screens/SplashScreen.dart
+++ lib/Screens/SplashScreen.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import 'Auth/LoginPage.dart';
+import 'NavBar.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -6,6 +10,30 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  FirebaseAuth mAuth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    new Future.delayed(Duration(seconds: 3), () async {
+      FirebaseUser user = await mAuth.currentUser();
+      if (user == null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => LoginPage(),
+          ),
+        );
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(),
+          ),
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
